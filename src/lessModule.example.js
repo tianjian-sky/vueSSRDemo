6b2d16ded6df66f3f9f41d6538b88c64db701199
虞天健 <gutakashiken@yutianjiandeMacBook-Pro.local>
2019-03-17 11:09:45 +0800

    update

---
+++ src/lessModule.example.js
@@ -0,0 +1,12 @@
+exports = module.exports = __webpack_require__( "./node_modules/css-loader/dist/runtime/api.js")(false);
+exports.push([module.i, "body {background: aquamarine;}", ""]);
+
+
+function (exports, require, module, __filename, __dirname) {
+    module.exports = (function(modules) { // webpackBootstrap    
+
+    }({
+        ...
+    })
+
+}
---
+++ src/vue.compiled.example.js
@@ -0,0 +1,20 @@
+__webpack_require__.r(__webpack_exports__);
+var _page_vue_vue_type_template_id_3cbd9fee_scoped_true___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__("./src/module/page.vue?vue&type=template&id=3cbd9fee&scoped=true&");
+var _page_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__("./src/module/page.vue?vue&type=script&lang=js&");
+var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__("./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
+function injectStyles (context) {
+    var style0 = __webpack_require__("./src/module/page.vue?vue&type=style&index=0&id=3cbd9fee&lang=less&scoped=true&")
+    if (style0.__inject__) style0.__inject__(context)
+}
+var component = Object(_node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__["default"])(
+        _page_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__["default"],
+        _page_vue_vue_type_template_id_3cbd9fee_scoped_true___WEBPACK_IMPORTED_MODULE_0__["render"],
+        _page_vue_vue_type_template_id_3cbd9fee_scoped_true___WEBPACK_IMPORTED_MODULE_0__["staticRenderFns"],
+        false,
+        injectStyles,
+        "3cbd9fee",
+        "cb5391c4"
+    )
+component.options.__file = "src/module/page.vue"
+ __webpack_exports__["default"] = (component.exports);
+ 
